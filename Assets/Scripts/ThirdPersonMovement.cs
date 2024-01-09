d40d91294fe4ce82413e6f94aa04ea6e8bf28267
Tyler Carrick <146818956+TylerCarrick@users.noreply.github.com>
2024-01-09 15:51:10 +0000

    Walking

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -6,14 +6,43 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+    public Animator anim;
     public CharacterController controller;
     public Transform cam;
     public float speed = 6;
-
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
     public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity; 
+    float turnSmoothVelocity;
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    Vector3 velocity;
+    bool isGrounded;
+    private void Start()
+    {
+        
+        Cursor.lockState = CursorLockMode.Locked;
+       
+    }
+
+
+
+
     void Update()
     {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+        anim.SetBool("Grounded", isGrounded);
+        if(isGrounded && velocity.y < 0 )
+        {
+            velocity.y = -2f;
+            anim.SetBool("Jump", false);
+
+
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -23,10 +52,33 @@ public class ThirdPersonMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            
+
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(direction * speed * Time.deltaTime);    
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            anim.SetBool("Walking", true);
         }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+       
+        controller.Move(velocity * Time.deltaTime);
+
+       if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            anim.SetBool("Jump", true);
+        }
+       
+
+    
+    
     }
-}
+
+    
+
+
+} 
 
